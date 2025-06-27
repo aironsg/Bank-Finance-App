9e615697cd5a76e32831e2de2b588627e981aa0b
DS Airon <airon_gm@hotmail.com>
2025-06-27 20:58:41 -0300

    feature: iniciando home do app

--- app/src/main/java/dev/airon/bankfinance/presenter/home/HomeFragment.kt
+++ app/src/main/java/dev/airon/bankfinance/presenter/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import dev.airon.bankfinance.R
 import dev.airon.bankfinance.databinding.FragmentHomeBinding
@@ -14,6 +15,7 @@ class HomeFragment : Fragment() {
 
     private var _binding : FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var bottomNavigationView: BottomNavigationView
 
 
 
@@ -23,6 +25,40 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBottomNavigation()
+    }
+
+    private fun  initBottomNavigation(){
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.navigation_home ->{
+                    true
+                }
+
+                R.id.navigation_charge_phone -> {
+
+                }
+
+                R.id.navigation_extract -> {
+
+                }
+
+                R.id.navigation_transfer -> {
+
+                }
+
+                R.id.navigation_profile -> {
+
+                }
+
+                else -> false
+            } as Boolean
+
+        }
     }
 
     override fun onDestroy() {
