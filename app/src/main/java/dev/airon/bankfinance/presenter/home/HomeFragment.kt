3ab93e6837be2f8b2614d2a4aeb7a82c59974953
DS Airon <airon_gm@hotmail.com>
2025-07-05 19:28:51 -0300

    modificando layout tela home, login

--- app/src/main/java/dev/airon/bankfinance/presenter/home/HomeFragment.kt
+++ app/src/main/java/dev/airon/bankfinance/presenter/home/HomeFragment.kt
@@ -8,10 +8,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import dagger.hilt.android.AndroidEntryPoint
 import dev.airon.bankfinance.R
 import dev.airon.bankfinance.data.model.Wallet
 import dev.airon.bankfinance.databinding.FragmentHomeBinding
+import dev.airon.bankfinance.util.FirebaseHelper
 import dev.airon.bankfinance.util.GetMask
 import dev.airon.bankfinance.util.StateView
 import dev.airon.bankfinance.util.showBottomSheet
@@ -37,7 +43,30 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getWallet()
+        getUsername()
         initNavigationDeposit()
+
+    }
+
+    private fun getUsername(){
+
+        val userId = FirebaseHelper.getUserId()
+        val nameRef = FirebaseDatabase.getInstance()
+            .getReference("profile")
+            .child(userId)
+            .child("name")
+
+        nameRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val name = snapshot.getValue(String::class.java)
+                binding.textUser.text = name ?: "Usuário não encontrado"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                binding.textUser.text = "Erro ao carregar nome"
+            }
+        })
+
 
     }
 
